51b4bf1ebf1a25cfb2f76ecec13e072995f46c52
tobuja48 <tobuja48@email.com>
2023-11-15 19:06:44 -0500

    data

--- Estilos/Data.js
+++ Estilos/Data.js
@@ -3,61 +3,73 @@ const data = [
       month: 1,
       consumoMensual: generateRandomConsumoArray(),
       numberOfDays: 31,
+      numberOfHoursPerDay: 24,
     },
     {
       month: 2,
       consumoMensual: generateRandomConsumoArray(),
       numberOfDays: 28,
+      numberOfHoursPerDay: 24,
     },
     {
       month: 3,
       consumoMensual: generateRandomConsumoArray(),
       numberOfDays: 31,
+      numberOfHoursPerDay: 24,
     },
     {
       month: 4,
       consumoMensual: generateRandomConsumoArray(),
       numberOfDays: 30,
+      numberOfHoursPerDay: 24,
     },
     {
       month: 5,
       consumoMensual: generateRandomConsumoArray(),
       numberOfDays: 31,
+      numberOfHoursPerDay: 24,
     },
     {
       month: 6,
       consumoMensual: generateRandomConsumoArray(),
       numberOfDays: 30,
+      numberOfHoursPerDay: 24,
     },
     {
       month: 7,
       consumoMensual: generateRandomConsumoArray(),
       numberOfDays: 31,
+      numberOfHoursPerDay: 24,
     },
     {
       month: 8,
       consumoMensual: generateRandomConsumoArray(),
       numberOfDays: 31,
+      numberOfHoursPerDay: 24,
     },
     {
       month: 9,
       consumoMensual: generateRandomConsumoArray(),
       numberOfDays: 30,
+      numberOfHoursPerDay: 24,
     },
     {
       month: 10,
       consumoMensual: generateRandomConsumoArray(),
       numberOfDays: 31,
+      numberOfHoursPerDay: 24,
     },
     {
       month: 11,
       consumoMensual: generateRandomConsumoArray(),
       numberOfDays: 30,
+      numberOfHoursPerDay: 24,
     },
     {
       month: 12,
       consumoMensual: generateRandomConsumoArray(),
       numberOfDays: 31,
+      numberOfHoursPerDay: 24,
     },
   ];
   
