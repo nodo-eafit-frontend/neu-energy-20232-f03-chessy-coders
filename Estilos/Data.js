9dd0f9865fe040a0cf52640519b19f5d143f34c8
Ronycore <127463614+Ronycore@users.noreply.github.com>
2023-11-16 20:00:52 -0500

    Cambios

--- Estilos/Data.js
+++ Estilos/Data.js
@@ -1,69 +1,17 @@
-const data = [
-    {
-      month: 1,
-      consumoMensual: generateRandomConsumoArray(),
-      numberOfDays: 31,
-    },
-    {
-      month: 2,
-      consumoMensual: generateRandomConsumoArray(),
-      numberOfDays: 28,
-    },
-    {
-      month: 3,
-      consumoMensual: generateRandomConsumoArray(),
-      numberOfDays: 31,
-    },
-    {
-      month: 4,
-      consumoMensual: generateRandomConsumoArray(),
-      numberOfDays: 30,
-    },
-    {
-      month: 5,
-      consumoMensual: generateRandomConsumoArray(),
-      numberOfDays: 31,
-    },
-    {
-      month: 6,
-      consumoMensual: generateRandomConsumoArray(),
-      numberOfDays: 30,
-    },
-    {
-      month: 7,
-      consumoMensual: generateRandomConsumoArray(),
-      numberOfDays: 31,
-    },
-    {
-      month: 8,
-      consumoMensual: generateRandomConsumoArray(),
-      numberOfDays: 31,
-    },
-    {
-      month: 9,
-      consumoMensual: generateRandomConsumoArray(),
-      numberOfDays: 30,
-    },
-    {
-      month: 10,
-      consumoMensual: generateRandomConsumoArray(),
-      numberOfDays: 31,
-    },
-    {
-      month: 11,
-      consumoMensual: generateRandomConsumoArray(),
-      numberOfDays: 30,
-    },
-    {
-      month: 12,
-      consumoMensual: generateRandomConsumoArray(),
-      numberOfDays: 31,
-    },
-  ];
-  
-  function generateRandomConsumoArray() {
-    return Array.from({ length: 24 }, () => Math.floor(Math.random() * 100));
-  }
-  
-  export default data;
-  
+[
+  {
+    "year": 2023,
+    "months": [
+      {
+        "mes": "enero",
+        "días": [
+          {"día": 1, "hora_x_energía": [167, 60, 96, 136, 134, 98, 126, 162, 57, 143, 86, 152, 178, 114, 174, 50, 124, 177, 148, 78, 88, 117, 111, 104]},
+          {"día": 2, "hora_x_energía": [165, 173, 108, 165, 116, 72, 121, 80, 157, 168, 80, 161, 115, 138, 67, 76, 107, 83, 178, 136, 123, 87, 146, 53]},
+          // ... (agrega los datos para los demás días del mes)
+        ]
+      },
+      // ... (agrega los datos para los demás meses del año)
+    ]
+  },
+  // ... (agrega los datos para los demás años)
+]
