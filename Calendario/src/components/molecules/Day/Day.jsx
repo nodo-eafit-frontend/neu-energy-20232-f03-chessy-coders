4afa1371afb3464afa21e6a42f5329f17a877e95
Ronycore <127463614+Ronycore@users.noreply.github.com>
2023-11-20 22:33:00 -0500

    Cambios

--- Calendario/src/components/molecules/Day/Day.jsx
+++ Calendario/src/components/molecules/Day/Day.jsx
@@ -1,30 +1,41 @@
 import React from 'react';
+import Hours from '../../atoms/Hours/Hours';
 import Label from '../../atoms/Label/Label';
 import Cell from '../../atoms/Cell/Cell';
 import energyData from '../../consumo_energia.json';
-import './Day.scss';
+import './Table.scss';
+
+// ...
+
+// ...
 
 const Day = () => {
-  // Accede a los datos de consumo de energía
   const monthlyData = energyData[0].months[0];
-
   const daysOfMonth = monthlyData.días.map(dayData => dayData.día);
   const hours = monthlyData.días[0].hora_x_energía.map((_, index) => index + 1);
 
   return (
-    <div className="day-container">
+    <div className="day-container" style={{ alignItems: 'flex-start' }}>
       <div className="labels">
+        <Hours />
         {daysOfMonth.map(day => (
-          <Label key={day} text={day.toString()} />
+          <Label key={day} text={day.toString()} className="day-label" />
         ))}
       </div>
       <div className="cells">
-        {hours.map(hour => (
-          <Cell key={hour} consumption={monthlyData.días[0].hora_x_energía[hour - 1]} />
+        <div className="hour-item"></div> {/* Espacio para la hora */}
+        {hours.map((hour, index) => (
+          <React.Fragment key={hour}>
+            <div className="hour-item">{hour}</div>
+            {monthlyData.días.map(dayData => (
+              <Cell key={`${dayData.día}-${hour}`} consumption={dayData.hora_x_energía[index]} />
+            ))}
+          </React.Fragment>
         ))}
       </div>
     </div>
   );
 };
 
-export default Day;
+// ...
+
