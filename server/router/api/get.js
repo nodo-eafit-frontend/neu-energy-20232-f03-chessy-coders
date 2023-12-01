6c5ee8b275fd0df11e76f0be36e0741268e8ac2f
juansebastian.moreno <juanmoreno.GLOB@commandalkon.com>
2023-11-30 21:07:18 -0500

    fix get callback

--- server/router/api/get.js
+++ server/router/api/get.js
@@ -2,70 +2,60 @@ const { leerJsonEnergia } = require('../../utils/json-update');
 require('dotenv').config();
 const { ENERGY_PATH } = process.env;
 
-  const getMaxEnergy = (monthFound) => {
+const getMaxEnergy = monthFound => {
     let maxEnergy = 0;
 
-    monthFound.days.forEach((day) => {
-      day.hora_x_energía.forEach((hour) => {
-        if (hour.energy > maxEnergy) {
-          maxEnergy = hour.energy;
-        }
-      });
+    monthFound.days.forEach(day => {
+        day.hora_x_energía.forEach(hour => {
+            if (hour > maxEnergy) {
+                maxEnergy = hour;
+            }
+        });
     });
 
     return maxEnergy;
-  };
-
-    const getConsuptionHour = (hours, maxEnergy) => {
-      const hora_x_energía = hours.map((energy) => {
-        return { energy, percent: energy / maxEnergy };
-      });
-
-      return hora_x_energía;
-    };
-
-  const injectEnergyPercent = (consumoMensual, maxEnergy) => {
-    const daysModified = consumoMensual.days.map((item) => {
-      const hora_x_energía = getConsuptionHour(item.hours, maxEnergy);
-
-      return { day: item.day, hora_x_energía };
-    });
-
-    return daysModified;
-  };
-
-const readEnergy = (req, res) => {
-  const { year, month } = req.params;
-
-  try {
-    // Zona segura
-    const consumo = leerJsonEnergia(ENERGY_PATH);
-
-    const yearFound = consumo.find((item) => item.year === +year);
-    const monthFound = yearFound.months.find((item) => item.month === +month);
-
-    // TODO:
-    // 1: Crear método que retorne el valor de enregía más grande getMaxEnergy(monthFound): number
-    const maxEnergy = getMaxEnergy(monthFound)
-    // 2: Modificar la respuesta de las horas, agregando el atributo de porcentaje injectEnergyPercent(monthFound, maxEnergy): monthFoundModified
-    const days = injectEnergyPercent(monthFound, maxEnergy)
-    
-    const consumoMensual = {
-      year,
-      month,
-      days,
-    };
-
-    if (monthFound) {
-      res.status(200).send(consumoMensual);
-    } else {
-      res.status(404).send({});
-    }
-  } catch (error) {
-    // Atrapamos el error
-    console.error(error);
-    res.status(500).send({});
-  }
 };
 
-module.exports = { readEnergy,};
+const injectEnergyPercent = (consumoMensual, maxEnergy) => {
+    consumoMensual.days.forEach(day => {
+        day.hora_x_energía = day.hora_x_energía
+            .map(value => ({ watts: value, percentage: value / maxEnergy }));
+    });
+    return consumoMensual.days;
+};
+
+const readEnergy = (req, res) => {
+    const { year, month } = req.params;
+
+    try {
+        // Zona segura
+        const consumo = leerJsonEnergia(ENERGY_PATH);
+
+        const yearFound = consumo.find(item => item.year === +year);
+        const monthFound = yearFound.months.find(item => item.month === +month);
+
+        // TODO:
+        // 1: Crear método que retorne el valor de energía más grande getMaxEnergy(monthFound): number
+        const maxEnergy = getMaxEnergy(monthFound);
+        // 2: Modificar la respuesta de las horas, agregando el atributo de porcentaje injectEnergyPercent(monthFound, maxEnergy): monthFoundModified
+        const days = injectEnergyPercent(monthFound, maxEnergy);
+
+        const consumoMensual = {
+            year,
+            month,
+            days
+        };
+
+        if (monthFound) {
+            res.status(200).send(consumoMensual);
+        } else {
+            res.status(404).send({});
+        }
+    } catch (error) {
+        // Atrapamos el error
+        console.error(error);
+        res.status(500).send({});
+    }
+};
+
+module.exports = { readEnergy };
